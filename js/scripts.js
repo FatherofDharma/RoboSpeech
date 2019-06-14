52e5c3af8ae94610eda49432d3b59eb3d9bc69bd
FatherofDharma <fatherofdharma@gmail.com>
2019-06-14 11:08:14 -0700

    add and remove comments from scripts.js and change function name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,13 @@
 //Business logic section of code.
 
 //This function creates an array containing the number range from 0 to the input number.
-// function numberRange(num) {
-// 	var range = []
-//   var dave = "I'm sorry, Dave. I'm afraid I can't do that."
-// 	for(var i = 0; i <= num; i++) {
-// 	   range.push(i)
-//   }
-//   return range
-// }
-// console.log(numberRange(23));
-
-function numberRange(num) {
+function convertNumber(num) {
   var range = [];
   var dave = " I'm sorry, Dave. I'm afraid I can't do that.";
   var boop = " boop!"
   var beep = " beep!"
   for (var i = 0; i <= num; i++) {
-    if (i.toString().match(/3/) != null) {
+    if (i.toString().match(/3/) != null) { //found this simple solution to finding a specific number in the string, on stack overflow.
       range.push(dave);
     } else if (i.toString().match(/2/) != null) {
         range.push(boop);
@@ -28,7 +18,7 @@ function numberRange(num) {
     }
   return range;
 }
-console.log(numberRange(23));
+
 
 
 
@@ -38,12 +28,10 @@ console.log(numberRange(23));
 //User Interface section of code.
 
 $(document).ready(function() {
-  console.log("hello from jquery");
   $("form#number").submit(function(event) {
       event.preventDefault();
       var input = $('#numberInput').val()
-      console.log(input);
-      var result = numberRange(input);
+      var result = convertNumber(input);
       $("#result").text(result);
   });
 });
