083c6f3d9e73001743ccc735ea4a256c5127dd9e
FatherofDharma <fatherofdharma@gmail.com>
2019-06-15 12:24:29 -0700

    lint the *&%$ out of my script.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -4,35 +4,44 @@
 function convertNumber(num) {
   var range = [];
   var userName = $('#username').val();
-  // var dave = " I'm sorry, " + userName + ", I'm afraid I can't do that.";
+
+  // var dave = ' I'm sorry, ' + userName + ', I'm afraid I can't do that.';
   // Using string interpolation instead of contatenation. Practice with friend.
   var dave = ` I'm sorry, ${userName}, I'm afraid I can't do that.`;
-  var boop = " Boop!";
-  var beep = " Beep!";
-  var zero = " Enter the void!";
+  var boop = ' Boop!';
+  var beep = ' Beep!';
+  var zero = ' Enter the void!';
+
   //This loop cycles from 0 to the users input number.
   for (var i = 0; i <= num; i++) {
-    //This identifies any numbers containing 3 and replaces it with "dave", 2's with "boop", and 1's with "beep."
-    if (i.toString().match(/3/) != null) { //found this simple solution to finding a specific number in a string, on stack overflow.
+
+    // This identifies any numbers containing 3 and replaces it with 'dave', 2's
+    // with 'boop', and 1's with 'beep.'
+    // I found this simple solution using to finding a specific number in a
+    //string, using .match, on stack overflow.
+    if (i.toString().match(/3/) != null) {
       range.push(dave);
     } else if (i.toString().match(/2/) != null) {
-        range.push(boop);
+      range.push(boop);
     } else if (i.toString().match(/1/) != null) {
-        range.push(beep);
+      range.push(beep);
     } else if (i.toString().match(/0/) != null) {
-        range.push(zero);
-    } else
-  	  range.push(" " + i);
+      range.push(zero);
+    } else {
+      range.push(' ' + i);
     }
+  }
+
   return range;
 }
+
 //User Interface section of code.
-$(document).ready(function() {
-  $("form#number").submit(function(event) {
+$(document).ready(function () {
+  $('form#number').submit(function (event) {
       event.preventDefault();
       var input = $('#numberInput').val();
       var result = convertNumber(input);
-      $("#result").text(result);
-      $("#halpic").show();
-  });
+      $('#result').text(result);
+      $('#halpic').show();
+    });
 });
